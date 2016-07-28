fa32deff6b14160c816b1a5093a5d318f917af21
qinaichen <qinaichen@alibaodu.com>
2016-07-28 15:40:08 +0800

    init

---
+++ src/main/java/com/baodu/wd/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.baodu.wd.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Zhipeng
+ * @date 2016/7/28.
+ */
+@Controller
+@RequestMapping(name = "/hello")
+public class HelloController {
+    @RequestMapping(name = "/")
+    @ResponseBody
+    public String hello(){
+        return "hello world!";
+    }
+}
